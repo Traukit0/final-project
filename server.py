d9351fa2e9497ada498d818c8d8f4c813a6ec805
Traukit0 <mcanonesbet@gmail.com>
2025-04-26 00:19:42 -0400

    modificado server.py paso 8

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+'''
+Deploy a Flask application that will allow a user to provide
+a text string which will then be analyzed to determine which emotion amongst a set of 5
+is the most likely emotion being conveyed by the given text.
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,14 +11,26 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def text_analyze():
+    '''
+    Retrieve the provided text string from the user, then pass the text
+    to be analyzed by the emotion detector. Finally, return a response displaying
+    the confidence scores across all emotions and the dominant emotion.
+    '''
     text = request.args.get("textToAnalyze")
     response = emotion_detector(text)
-    if response['dominant_emotion'] == None:
+    if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    return f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']}, and 'sadness': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}"
+    return f"""For the given statement, the system response is
+            'anger': {response['anger']}, 'disgust': {response['disgust']},
+            'fear': {response['fear']}, 'joy': {response['joy']}, and 'sadness': {response['sadness']}.
+            The dominant emotion is {response['dominant_emotion']}"""
 
 @app.route("/")
 def render_index_page():
+    '''
+    Render the index page to the user, this is where the text string to be
+    analyzed is provided and a response is displayed back to the user.
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
